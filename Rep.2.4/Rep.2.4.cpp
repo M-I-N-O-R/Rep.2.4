b564420aff31f45e56e42a29418a5b268609d59c
Олександр <goncars451@gmail.com>
2024-10-04 22:48:56 +0300

    Добавьте файлы проекта.

---
+++ Rep.2.4/Rep.2.4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Введіть число N: ";
+    cin >> n;
+
+    // Перевірка кожного числа від 2 до N
+    for (int i = 2; i <= n; ++i) {
+        bool isPrime = true;  // Припускаємо, що число просте
+
+        // Перевірка, чи є число i простим
+        for (int j = 2; j * j <= i; ++j) {
+            if (i % j == 0) {
+                isPrime = false;  // Якщо знайдено дільник, то число не є простим
+                break;            // Вихід з внутрішнього циклу
+            }
+        }
+
+        // Якщо isPrime залишилось true, то це просте число
+        if (isPrime) {
+            cout << i << " ";
+        }
+    }
+
+    cout << endl;
+    return 0;
+}
